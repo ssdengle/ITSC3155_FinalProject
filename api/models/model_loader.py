9340ebba68205f0dd46576267d7e88442ed7ea6b
SriVM <sdengle@charlotte.edu>
2026-04-15 21:41:32 -0400

    model_loader.py updated and added new tables

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,11 +1,29 @@
-from . import orders, order_details, recipes, sandwiches, resources
+# from . import orders, order_details, recipes, sandwiches, resources
+
+# from ..dependencies.database import engine
+
+
+# def index():
+#     orders.Base.metadata.create_all(engine)
+#     order_details.Base.metadata.create_all(engine)
+#     recipes.Base.metadata.create_all(engine)
+#     sandwiches.Base.metadata.create_all(engine)
+#     resources.Base.metadata.create_all(engine)
+from . import (
+    orders,
+    order_details,
+    recipes,
+    sandwiches,
+    resources,
+    customers,
+    payments,
+    reviews,
+    promotions,
+    order_promotions
+)
 
 from ..dependencies.database import engine
 
 
 def index():
-    orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+    orders.Base.metadata.create_all(bind=engine)
